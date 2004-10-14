9ed095941cb1f4690259b386225a70fe51643131
tegen <tegen@eaea48a0-baa0-8274-1f2b-b4cd8b5cfcb2>
2004-10-14 14:48:02 +0000

    Main doxypage...

---
+++ mainpage.h
@@ -0,0 +1,13 @@
+// Mainpage for Doxygen
+
+/** @mainpage package PoleLib
+ *
+ * @section intro Introduction
+ *
+ * @author Fredrik Tegenfeldt, Iowa State University (fredrik.tegenfeldt@cern.ch)
+ *
+ *<hr>
+ * @section notes  Notes for current release
+ * release.notes
+ *<hr>
+ */
