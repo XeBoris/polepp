5c9e57154262290235bad5a412dd95e01e034474
tegen <tegen@eaea48a0-baa0-8274-1f2b-b4cd8b5cfcb2>
2005-07-11 20:37:00 +0000

    implemented copy constructor/operator

--- Random.h
+++ Random.h
@@ -12,17 +12,18 @@
 class Random {
 protected:
   unsigned int m_seed;  //Random number generator seed
-  double m_mean;
-  double m_sigma;
 public:
   Random(unsigned int seed=65539);
   virtual ~Random();
+  void copy(const Random &rnd) { m_seed = rnd.getSeed(); }
+  Random & operator=(const Random &rnd) { copy(rnd); return *this; }
+      //
   double gaus(double mean=0.0, double sigma=1.0);
   double logNormal(double mean=1.0, double sigma=1.0);
   int    poisson(double mean);
   double flat(double mean, double sigma=1.0);
   double general(int npts, double *x, double xmin, double xmax, double *f, double fmin, double fmax);
-  unsigned int getSeed() {return m_seed;}
+  const unsigned int getSeed() const {return m_seed;}
   void   setSeed(unsigned int seed=65539);
   //
   virtual double rndm();
