669d2a48b5aeeaa2d12e3717dbc946698a6c9541
tegen <tegen@eaea48a0-baa0-8274-1f2b-b4cd8b5cfcb2>
2006-11-20 10:18:43 +0000

    new methods

--- Combination.h
+++ Combination.h
@@ -4,10 +4,17 @@
 #include <vector>
 
 namespace Combination {
-  bool add(std::vector<int> & cnt, int elem, int vmax);
-  bool add(std::vector<int> & cnt, int elem, const std::vector<int> & vmax);
-  bool next_vector(std::vector<int> & vec, int vmax);
-  bool next_vector(std::vector<int> & vec, const std::vector<int> & vmax);
+  inline bool add(std::vector<int> & cnt, int elem, int vmax);
+  inline bool add(std::vector<int> & cnt, int elem, const std::vector<int> & vmax);
+  inline bool next_vector(std::vector<int> & vec, int vmax);
+  inline bool next_vector(std::vector<int> & vec, const std::vector<int> & vmax);
+  //
+  inline int  makeIndexVector( int imax, std::vector< std::vector<int> > & nvec);
+  inline int  makeIndexVector( const std::vector<int> & imax, std::vector< std::vector<int> > & nvec);
+  inline int  getIndexVector(std::vector<int> & nvec, std::vector< std::vector<int> > & allVectors);
+
 }; // end of Utils
+
+#include "Combination.icc"
 
 #endif
